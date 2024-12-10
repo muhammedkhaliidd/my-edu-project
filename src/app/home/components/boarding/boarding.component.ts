97b42c1008e0ab8392be88b4083605c13557a1dc
muhammedkhaliddyne <mohamed.khaled@dyne-app.com>
2024-12-10 09:18:49 +0200

    Added design of boarding component

--- src/app/home/components/boarding/boarding.component.ts
+++ src/app/home/components/boarding/boarding.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-boarding',
   templateUrl: './boarding.component.html',
   styleUrls: ['./boarding.component.scss'],
   standalone: true,
+  imports: [IonButton],
 })
 export class BoardingComponent implements OnInit {
   constructor() {}
